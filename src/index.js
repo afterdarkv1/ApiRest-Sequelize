3785904863eb438a4536bb22712244e4d3694040
Luis <bamcuent312@gmail.com>
2023-10-02 15:35:43 +0200

    config to railway

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 
 import { app } from "./app.js";
+import { PORT } from "./config.js";
 import database from "./db/db.js";
 
 async function main() {
   await database.sync();
-  app.listen(4002);
-  console.log("Server on port 4002");
+  app.listen(PORT);
+  console.log(`Server on port http://localhost:${PORT}`);
 }
 
 main();
